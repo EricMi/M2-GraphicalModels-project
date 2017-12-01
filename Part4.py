4ee05e2a78f3acf5bc064073084cc8a685ee9fcc
EricMi <mipengfei1995@gmail.com>
2017-12-01 18:30:22 +0100

    Part5 code done. Now we get the smoothing distribution.

--- Part4.py
+++ Part4.py
@@ -1,7 +1,10 @@
 # Part 4: Compute the filtering distribution via Forward recursion
+# For numerical stability
 eps = np.finfo(float).eps
 p_init[p_init < eps] = eps
 
+# log_alpha: update message in forward pass 
+# log_alpha_predict: predict message in forward pass
 log_alpha = np.zeros(NumStates, T)
 log_alpha_predict = np.zeros((NumStates, T))
 for t in range(T):
@@ -13,6 +16,18 @@ for t in range(T):
         log_alpha_predict[:, t] = np.log(A.dot(p)) + mx
     log_alpha[:, t] = log_alpha_predict[:, t] + logObs[:, t]
 
-d_filtering = np.zeros((NumStates, T))
+# d_filtering: filtering distribution
+d_filtering = nip.zeros((NumStates, T))
 for t in range(T):
-    d_filtering[:, t] = log_alpha[:, t] / np.sum(log_alpha[:, t])
+    alpha = np.exp(log_alpha[:, t])
+    d_filtering[:, t] = alpha / np.sum(alpha)
+
+# d_marginal_filtering_s: marginal filtering distribution of s
+# d_marginal_filtering_c: marginal filtering distribution of c
+# d_marginal_filtering_m: marginal filtering distribution of m
+d_marginal_filtering_s = np.zeros((S, T))
+d_marginal_filtering_c = np.zeros((C, T))
+d_marginal_filtering_m = np.zeros((M, T))
+for t in range(T):
+    state_t = [States[i] for i in ]
+    d_marginal_filtering_s = np.sum()
---
+++ Part5.py
@@ -0,0 +1,20 @@
+# Part 5: Compute the smoothing distribution via Forward-Backward recursion
+
+# log_beta: update message in backward pass
+# log_beta_postdict: postdict message in backward pass
+log_beta = np.zeros((NumStates, T))
+log_beta_postdict = np.zeros((NumStates, T))
+for t in range(T-1:-1:-1):
+    if t == T-1:
+        log_beta_postdict[:, t] = np.zeros(NumStates)
+    else:
+        mx = np.max(log_beta[:, t + 1])
+        p = np.exp(log_beta[:, t + 1] - mx)
+        log_beta_postdict[:, t] = log(A.T.dot(p)) + mx
+    log_beta[:, t] = log_beta_postdict[:, t] + logObs[:, t]
+
+# d_smoothing: smoothing distribution
+d_smoothing = np.zeros((NumStates, T))
+for t in range(T):
+    d_smoothing = np.exp(log_alpha[:, t] + log_beta_postdict[:, t])
+
